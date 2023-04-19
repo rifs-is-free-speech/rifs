96e371c3134ea726844c931dd605d8666b204145
Emil Joensen <emiljoesen@hotmail.com>
2023-04-19 12:18:35 +0200

    Remove line

--- src/rifs/fairseq.py
+++ src/rifs/fairseq.py
@@ -72,7 +72,6 @@ def fairseq_constructor(
     str
     """
     k = 1
-    # TODO: Give this as a parameter
     label_path = "?"
     command_path = join(fairseq_path, model_dict["command"])
     command = f"{command_path} "
